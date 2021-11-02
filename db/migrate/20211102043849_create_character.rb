4f344e8d7adb6f877ca2e0078b289481ae014402
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-11-02 01:08:08 -0400

    Commit 4: Renamed the column

--- db/migrate/20211102043849_create_character.rb
+++ db/migrate/20211102043849_create_character.rb
@@ -1,7 +1,9 @@
 class CreateCharacter < ActiveRecord::Migration[7.0]
   def change
     create_table :characters do |t|
-
+      t.string :name
+      t.string :description
+      t.string :img_url
       t.timestamps
     end
   end
