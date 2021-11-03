7e00857342d90d48c47960e65ea0e42d48e631ab
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-11-02 23:48:11 -0400

    Commit 9: Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "In his normal life, he is a quiet programmer for the  Meta Cortex; while in private, he is a computer hacker who penetrates computer systems illicitly and steals information under his hacker alias Neo."},
+	{ name: "Trinity", description: "Trinity is a computer programmer and a hacker who has escaped from the Matrix, a sophisticated computer program where most humans are imprisoned. " },
+	{ name: "Morpheus", description: "Morpheus is the captain of the Nebuchadnezzar, which is a hovercraft of the human forces of the last human city, Zion, in a devastated world where most humans are grown by sentient machines to be used as power sources and their minds kept imprisoned in the Matrix, a virtual computer-generated world, to stop them from realising the truth of the real world." },
+	{ name: "Agent Smith", description: "Smith began as an Agent, an AI program in the Matrix programmed to keep order within the system by terminating human simulacra which would bring instability to the simulated reality, as well as any rogue programs that no longer serve a purpose to the Machine collective." },
 ])
 
 # This file should contain all the record creation needed to seed the database with its default values.
